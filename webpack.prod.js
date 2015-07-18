ffbab6fb197375d2eba71c17207fe4fa59fa8edb
Jorin Vogel <hi@jorin.me>
2015-07-18 14:17:09 +0700

    Added `npm run build-dev` command.

--- webpack.prod.js
+++ webpack.prod.js
@@ -14,7 +14,7 @@ module.exports = {
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': '"production"'
+        'NODE_ENV': '"' + (process.env.CLIENT_ENV || 'production') + '"'
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
